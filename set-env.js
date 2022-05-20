bc4484bd804873b4ffc841e09107a450bdb675d3
Viktor Tsvetkov <11594423+Tsvetkovv@users.noreply.github.com>
2022-05-20 15:47:06 +0200

    fix: build command

--- set-env.js
+++ set-env.js
@@ -7,14 +7,19 @@ require('dotenv').config();
 // Would be passed to script like this:
 // `node set-env.js --environment=dev`
 // we get it from yargs's argv object
+console.log(argv);
 const environment = argv.environment;
-const isProd = environment === 'prod';
+const isProd = environment !== 'development';
 
 // Configure Angular `environment.ts` file path
-const targetPath = isProd
-  ? `./src/environments/environment.prod.ts`
-  : `./src/environments/environment.ts`;
-// `environment.ts` file structure
+const devFileName = `./src/environments/environment.ts`;
+const prodFileName = `./src/environments/environment.prod.ts`;
+const targetPath = isProd ? prodFileName : devFileName;
+if (isProd) {
+  // non prod file should exist
+  fs.writeFileSync(devFileName, '');
+}
+
 const apiKey = process.env['GIPHY_API_KEY'];
 invariant(apiKey, 'GIPHY_API_KEY must be specified');
 
